4cc823808a58c92c26af47d889f01bf292d5d484
Lucas Oliveira <contato@devlucas.com.br>
2014-12-01 02:51:00 -0200

    Add the license head to some .php files.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
 namespace Igd\CodeceptionBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
--- IgdCodeceptionBundle.php
+++ IgdCodeceptionBundle.php
@@ -1,9 +1,16 @@
 <?php
-
+/**
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
 namespace Igd\CodeceptionBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * {@inheritdoc}
+ */
 class IgdCodeceptionBundle extends Bundle
 {
 }
